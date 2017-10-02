20512ed6088d868595588c5d4fd58baf12f9e2ce
James Abney <james.abney@gmail.com>
2017-10-02 00:47:55 -0700

    Remove valueIndicatorPlugin

--- server/models/item.model.ts
+++ server/models/item.model.ts
@@ -1,18 +1,18 @@
 import * as mongoose from 'mongoose'
 import * as timestampsPlugin from 'mongoose-times'
 // import { updatedOnPlugin } from '../plugins/updated-on'
-import { valueIndicatorPlugin } from '../plugins/value-indicator'
+// import { valueIndicatorPlugin } from '../plugins/value-indicator'
 
 const itemSchema = new mongoose.Schema({
     name: String,
     description: String,
     category: String,
     estvalue: Number
-}, { strict: false })
+})
 
 // itemSchema.plugin(updatedOnPlugin)
 itemSchema.plugin(timestampsPlugin)
-itemSchema.plugin(valueIndicatorPlugin)
+// itemSchema.plugin(valueIndicatorPlugin)
 
 const Item = mongoose.model('Item', itemSchema)
 
