1507d3152bf72b9411d63f058b2049eb9eb84092
James Abney <james.abney@gmail.com>
2017-10-01 17:26:46 -0700

    Add validate middleware for estvalue field

--- server/models/item.model.ts
+++ server/models/item.model.ts
@@ -5,8 +5,13 @@ const itemSchema = mongoose.Schema({
     description: String,
     category: String,
     estvalue: Number
-})
+}, { strict: false })
 
 const Item = mongoose.model('Item', itemSchema)
+
+// Custom validator
+Item.schema.path('estvalue').validate((value) => {
+    return value > 0
+}, 'Estimated value must be greater than zero')
 
 export { Item }
